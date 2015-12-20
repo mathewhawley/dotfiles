4af06a3bf5803ff62f01ccb4707606a55184fe8e
Mathew Hawley <mathew@mathewhawley.com>
2015-12-20 11:02:12 +0000

    Symlink '.zshrc'

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@ BASE=$(pwd)
 # get permissions
 sudo -v
 
-# make backup of .vimrc
+# make backups
 mv -v ~/.vimrc ~/.vimrc-old
+mv -v ~/.zshrc ~/.zshrc-old
 
 # create symlinks
 ln -sf $BASE/.vimrc ~/.vimrc
 ln -sf $BASE/zsh/custom.zsh-theme ~/.oh-my-zsh/themes
+ln -sf $BASE/.zshrc ~/.zshrc
