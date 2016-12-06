b970974148a9a3f4a3ca04eb2403a9cd983d574f
Mathew Hawley <mathew@mathewhawley.com>
2016-12-06 20:32:00 +0000

    Re-organise 'install.sh', add '.editorconfig'

--- install.sh
+++ install.sh
@@ -1,28 +1,65 @@
 #!/usr/bin/env bash
 
-BASE=$(pwd)
-
 # Get permissions
 sudo -v
 
-# Make temp file directories for vim
-run "Creating Vim temporary directories..."
-mkdir -pv ~/.vim/swaps ~/.vim/backups ~/.vim/undo
-echo "Done."
+# Load functions
+source ./bin/echoes
+source ./bin/helpers
 
-# Make backups
-echo "Backing up '.vimrc' and '.zshrc'..."
-cp -nv ~/.vimrc ~/.vimrc-old
-cp -nv ~/.zshrc ~/.zshrc-old
-echo "Done."
+callout "Running set up script"
 
-Create symlinks
-echo "Creating symlinks..."
-ln -sf $BASE/vim/.vimrc ~/.vimrc
+# Handle Homebrew install/update
+which brew 2>&1 > /dev/null
+if [[ $? -ne 0 ]]; then
+  task "Installing Homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  if [[ $? -ne 0 ]]; then
+    error "Unable to install Homebrew"
+    exit 2
+  else
+    ok
+  fi
+else
+  task "Updating Homebrew"
+  brew update > /dev/null
+  ok
+  prompt "Upgrade outdated packages (brew upgrade)? [y|N]"
+  read response
+  if [[ $response =~ ^(y|yes|Y) ]]; then
+    task "Upgrading Homebrew packages"
+    brew upgrade
+    ok
+  else
+    task "Skipped Homebrew package upgrade"
+    ok
+  fi
+fi
+
+# Set up .gitlocal
+if ! [[ -f ~/.gitlocal ]]; then
+  warn "It seems you don't have a '.gitlocal' set up yet"
+  prompt "What is your name?"
+  echo ""
+  read -r name
+  prompt "What is your email address?"
+  echo ""
+  read -r email
+  task "Updating '.gitlocal'"
+  git config -f ~/.gitlocal user.name "${name}"
+  git config -f ~/.gitlocal user.email "${email}"
+  ok
+fi
+
+# Create symlinks
+task "Creating symlinks"
+BASE=$(pwd)
 ln -sf $BASE/zsh/.zshrc ~/.zshrc
 ln -sf $BASE/git/.gitignore_global ~/.gitignore_global
 ln -sf $BASE/git/.gitconfig ~/.gitconfig
 ln -sf $BASE/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 ln -sf $BASE/sublime/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 ln -sf $BASE/sublime/Material-Theme.sublime-theme ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-echo "Done."
+ok
+
+callout "Complete!"
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,33 +1,26 @@
 # PATH
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 
-# Path to on-my-zsh installation
+# ZSH
 export ZSH=$HOME/.oh-my-zsh
+# NVM
+export NVM_DIR="$HOME/.nvm"
 
 # Colourize GREP output
 export GREP_OPTIONS="--color=auto"
-
 # Default editor
 export EDITOR=subl
 
-# Set NVM directory
-export NVM_DIR="$HOME/.nvm"
-
-# Load NVM
-if [[ -f "$NVM_DIR/nvm.sh" ]]; then
-  . "$NVM_DIR/nvm.sh"
-fi
-
-# Load fzf
-if [[ -f ~/.fzf.zsh ]]; then
-  source ~/.fzf.zsh
-fi
+# Include helper functions
+source ~/.dotfiles/bin/helpers
 
 # Load oh-my-zsh
-source $ZSH/oh-my-zsh.sh
-
-# Load CLI aliases
-source ~/.dotfiles/zsh/aliases
-
+require $ZSH/oh-my-zsh.sh
 # Load prompt config
-source ~/.dotfiles/zsh/prompt
+require ~/.dotfiles/zsh/prompt
+# Load NVM
+require "$NVM_DIR/nvm.sh"
+# Load fzf
+require ~/.fzf.zsh
+# Load aliases
+require ~/.dotfiles/zsh/aliases
